b1ca0355aa72264d31e22df20846461655000132
unknown <taewoo1212@gmail>
2023-12-19 08:59:32 +0900

    저장

--- client/main.js
+++ client/main.js
@@ -1,6 +1,4 @@
-
-
-//  모듈 프로그래밍 
+//  모듈 프로그래밍
 
 // named export (이름 내보내기) -> { 이름 }
 // default export (기본 내보내기) -> 이름
@@ -9,21 +7,13 @@
 // import { insertLast } from "./lib/dom/insert.js";
 // import clearContents from "./lib/dom/clear.js";
 
-import {
-  getNode, 
-  insertLast,
-  clearContents,
-  getNodes, 
-} from './lib/index.js';
+import { getNode, insertLast, clearContents, getNodes } from "./lib/index.js";
 
-
-function phase1(){
-
-  const first = getNode('#firstNumber');
-  const second = getNode('#secondNumber');
-  const result = getNode('.result');
-  const clear = getNode('#clear');
-
+function phase1() {
+  const first = getNode("#firstNumber");
+  const second = getNode("#secondNumber");
+  const result = getNode(".result");
+  const clear = getNode("#clear");
 
   // 1. input value 값 가져오기
   //     - input에게 input 이벤트를 걸어 준다.
@@ -32,88 +22,52 @@ function phase1(){
   // 2. 두 수의 합 더하기
   //     - 타입 확인하기.
 
-
   // 3. 합계 랜더링 하기
   //     - insertLast(), insertAdjacentHTML()
 
-
-  function handleInput(){
+  function handleInput() {
     const firstValue = first.value * 1;
     const secondValue = second.value / 1;
-    const total = firstValue + secondValue;
 
+    const total = firstValue + secondValue;
 
     // result.textContent = ''
 
-    clearContents(result)
-    insertLast(result,total)
-    
+    clearContents(result);
+    insertLast(result, total);
   }
 
-
-  function handleClear(e){
+  function handleClear(e) {
     e.preventDefault();
-    clearContents(first)
-    clearContents(second)
-    result.textContent = '-'
+    clearContents(first);
+    clearContents(second);
+    result.textContent = "-";
   }
 
-
-
-  first.addEventListener('input',handleInput)
-  second.addEventListener('input',handleInput)
-  clear.addEventListener('click',handleClear)
-
-
+  first.addEventListener("input", handleInput);
+  second.addEventListener("input", handleInput);
+  clear.addEventListener("click", handleClear);
 }
-
-
-
 
 // 이벤트 위임 처리
 
+const calculator = getNode(".calculator");
+const result = getNode(".result");
+const clear = getNode("#clear");
+const numberInputs = Array.from(getNodes("input:not(#clear)"));
 
-
-const calculator = getNode('.calculator');
-const result = getNode('.result');
-const clear = getNode('#clear');
-const numberInputs = Array.from(getNodes('input:not(#clear)'));
-
-
-function handleInput(){
-  const total = numberInputs.reduce((acc,cur)=> acc + Number(cur.value) ,0);
-  clearContents(result)
-  insertLast(result,total);
+function handleInput() {
+  const total = numberInputs.reduce((acc, cur) => acc + Number(cur.value), 0);
+  clearContents(result);
+  insertLast(result, total);
 }
 
-
-function handleClear(e){
+function handleClear(e) {
   e.preventDefault();
   numberInputs.forEach(clearContents);
 
-  result.textContent = '-'
+  result.textContent = "-";
 }
 
-
-calculator.addEventListener('input',handleInput);
-clear.addEventListener('click',handleClear);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+calculator.addEventListener("input", handleInput);
+clear.addEventListener("click", handleClear);
